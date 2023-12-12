a1232dc597d421e8ba0e97221ad390c2b2144dbd
George Tian <github@georgetian.com>
2023-12-12 22:15:00 +0800

    2023-12-12 22:15:00

--- headers/collidable.hpp
+++ headers/collidable.hpp
@@ -8,13 +8,19 @@
 #include <glm/glm.hpp>
 
 
+
+
 struct Collision {
-    bool collided;
-    unsigned int id1;
-    unsigned int id2;
-    glm::dvec3 point;
+    const unsigned int id1;
+    const unsigned int id2;
+    const glm::dvec3 point;
+    Collision(const unsigned int id1, const unsigned int id2, const glm::dvec3 point): id1(id1), id2(id2), point(point) {}
+    static Collision isNoCollision() const {
+        return id1 == 0 && id2 == 0;
+    }
 };
 
+const Collision NO_COLLISION = Collision(0, 0, glm::dvec3(0.0));
 
 class Sphere;
 class RectangularCuboid;
@@ -41,15 +47,20 @@ public:
     glm::dvec3 getMaxAABB() const;
     static std::vector<std::shared_ptr<Collidable>> readConfig(const std::filesystem::path& path);
     virtual std::string toString() const = 0;
-    // static Collision collide(const Sphere&, const Sphere&);
-    // static Collision collide(const Sphere&, const RectangularCuboid&);
-    // static Collision collide(const Sphere&, const Cube&);
-    // static Collision collide(const Cube&, const Sphere&);
-    // static Collision collide(const Cube&, const Sphere&);
-    // static Collision collide(const Cube&, const Sphere&);
-    // static Collision collide(const RectangularCuboid&, const Sphere&);
-    // static Collision collide(const RectangularCuboid&, const Cube&);
-    // static Collision collide(const RectangularCuboid&, const RectangularCuboid&);
+    static Collision collide(const Sphere& a, const Sphere& b) {
+        if (a.getRadius() * a.getRadius() + b.getRadius() * b.getRadius() > glm::length2(a.getCenter() - b.getCenter())) {
+
+        }
+
+    }
+    static Collision collide(const Sphere&, const RectangularCuboid&);
+    static Collision collide(const Sphere&, const Cube&);
+    static Collision collide(const Cube&, const Sphere&);
+    static Collision collide(const Cube&, const Sphere&);
+    static Collision collide(const Cube&, const Sphere&);
+    static Collision collide(const RectangularCuboid&, const Sphere&);
+    static Collision collide(const RectangularCuboid&, const Cube&);
+    static Collision collide(const RectangularCuboid&, const RectangularCuboid&);
 
 };
 
