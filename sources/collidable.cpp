b5c31e7b48fa49e350b7e03dbb3eb5f3d073f485
George Tian <github@georgetian.com>
2023-12-12 14:54:15 +0800

    2023-12-12 14:54:15

--- sources/collidable.cpp
+++ sources/collidable.cpp
@@ -63,6 +63,7 @@ std::vector<std::shared_ptr<Collidable>> Collidable::readConfig(const std::files
 }
 
 Sphere::Sphere(const glm::dvec3 center, const double radius): radius(radius) {
+    std::cout << "r " << radius << '\n';
     position = center;
     minAABB = glm::dvec3(-radius / 2);
     maxAABB = glm::dvec3(+radius / 2);
