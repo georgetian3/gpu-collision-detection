fdce76ee703b16dd97233206d5158a7afb6fb846
George Tian <github@georgetian.com>
2023-12-12 10:17:22 +0800

    2023-12-12 10:17:22

---
+++ headers/collision_detector.hpp
@@ -0,0 +1,16 @@
+#ifndef COLLISION_DETECTOR_HPP
+#define COLLISION_DETECTOR_HPP
+
+#include <collidable.hpp>
+
+class CollisionDetector {
+
+
+public:
+
+    void addCollidable(const Collidable& collidable);
+
+
+};
+
+#endif
---
+++ headers/stopwatch.hpp
@@ -0,0 +1,64 @@
+#ifndef STOPWATCH_HPP
+#define STOPWATCH_HPP
+
+#include <chrono>
+
+class Stopwatch {
+
+    std::chrono::high_resolution_clock clock;
+    std::chrono::time_point<std::chrono::high_resolution_clock> start_time;
+    double elapsed_seconds = 0;
+
+    enum class State {
+        init,
+        started,
+        stopped
+    };
+
+    Stopwatch::State state = State::init;
+
+    double elapsed_since_start() const {
+        return std::chrono::duration<double>(clock.now() - start_time).count();
+    }
+
+public:
+
+    Stopwatch() {
+        reset();
+    }
+
+    void start() {
+        if (state == State::started) {
+            return;
+        }
+        state = State::started;
+        start_time = clock.now();
+    }
+
+    double stop() {
+        if (state != State::started) {
+            return -1;
+        }
+        elapsed_seconds += elapsed_since_start();
+        state = State::stopped;
+        return elapsed();
+    }
+
+    void reset() {
+        elapsed_seconds = 0;
+        state = State::init;
+    }
+
+    double elapsed() const {
+        if (state == State::started) {
+            return elapsed_since_start() + elapsed_seconds;
+        }
+        if (state == State::stopped) {
+            return elapsed_seconds;
+        }
+        return -1;
+    }
+
+};
+
+#endif
