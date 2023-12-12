0085d1bff20b3d28e673ec145c5b62a923c07758
George Tian <github@georgetian.com>
2023-12-12 23:44:33 +0800

    2023-12-12 23:44:33

--- sources/main.cpp
+++ sources/main.cpp
@@ -7,10 +7,10 @@ int main(int argc, char* argv[]) {
     if (argc > 1) {
         std::cout << "Command line arguments ignored\n";
     }
-    // auto collidables = Collidable::readConfig("resources/collidables.txt");
-    // for (int i = 0; i < collidables.size(); i++) {
-    //     std::cout << collidables[i]->toString() << '\n';
-    // }
+    auto collidables = Collidable::readConfig("resources/collidables.txt");
+    for (int i = 0; i < collidables.size(); i++) {
+        std::cout << collidables[i]->toString() << '\n';
+    }
     NaiveCollisionDetector naiveCD;
     // Scene scene(
     //     (std::filesystem::current_path() / std::filesystem::path(argv[0]).parent_path()).lexically_normal(),
