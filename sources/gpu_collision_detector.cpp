a028f742858b6264dffd41791ba82ca50c7d3138
George Tian <github@georgetian.com>
2023-12-13 21:11:10 +0800

    2023-12-13 21:11:10

--- sources/gpu_collision_detector.cpp
+++ sources/gpu_collision_detector.cpp
@@ -184,24 +184,24 @@ GpuCollisionDetector::GpuCollisionDetector() {
  
  
     // // //run the kernel
-    cl::KernelFunctor simple_add(cl::Kernel(program,"simple_add"),queue,cl::NullRange,cl::NDRange(10),cl::NullRange);
-    simple_add(buffer_A,buffer_B,buffer_C);
+    // cl::KernelFunctor simple_add(cl::Kernel(program,"simple_add"),queue,cl::NullRange,cl::NDRange(10),cl::NullRange);
+    // simple_add(buffer_A,buffer_B,buffer_C);
  
-    // // //alternative way to run the kernel
-    // // /*cl::Kernel kernel_add=cl::Kernel(program,"simple_add");
-    // // kernel_add.setArg(0,buffer_A);
-    // // kernel_add.setArg(1,buffer_B);
-    // // kernel_add.setArg(2,buffer_C);
-    // // queue.enqueueNDRangeKernel(kernel_add,cl::NullRange,cl::NDRange(10),cl::NullRange);
-    // // queue.finish();*/
+    //alternative way to run the kernel
+    cl::Kernel kernel_add=cl::Kernel(program,"simple_add");
+    kernel_add.setArg(0,buffer_A);
+    kernel_add.setArg(1,buffer_B);
+    kernel_add.setArg(2,buffer_C);
+    queue.enqueueNDRangeKernel(kernel_add,cl::NullRange,cl::NDRange(10),cl::NullRange);
+    queue.finish();
  
-    // // int C[10];
-    // // //read result C from the device to array C
-    // // queue.enqueueReadBuffer(buffer_C,CL_TRUE,0,sizeof(int)*10,C);
+    int C[10];
+    //read result C from the device to array C
+    queue.enqueueReadBuffer(buffer_C,CL_TRUE,0,sizeof(int)*10,C);
  
-    // // std::cout<<" result: \n";
-    // // for(int i=0;i<10;i++){
-    // //     std::cout<<C[i]<<" ";
-    // // }
+    std::cout<<" result: \n";
+    for(int i=0;i<10;i++){
+        std::cout<<C[i]<<" ";
+    }
 
 }
