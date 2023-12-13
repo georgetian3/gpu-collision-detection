bc663df6daaf41b6712018218a4d0576623147ab
George Tian <github@georgetian.com>
2023-12-13 23:26:25 +0800

    2023-12-13 23:26:25

--- sources/gpu_collision_detector.cpp
+++ sources/gpu_collision_detector.cpp
@@ -2,6 +2,7 @@
 #define CL_HPP_ENABLE_EXCEPTIONS
 #define CL_HPP_TARGET_OPENCL_VERSION 300
 #include <CL/opencl.hpp>
+#include <exception>
 #include <fstream>
 #include <sstream>
 #include <json.hpp>
