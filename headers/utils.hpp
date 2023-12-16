5f947d77deb2d7092e52a7b00468ad412cc69cf9
George Tian <github@georgetian.com>
2023-12-16 18:14:44 +0800

    2023-12-16 18:14:44

--- headers/utils.hpp
+++ headers/utils.hpp
@@ -15,8 +15,18 @@ T clamp(T value, T min, T max) {
 
 // https://stackoverflow.com/a/35687575
 template<typename Numeric, typename Generator = std::mt19937>
-Numeric random(Numeric from, Numeric to);
-
+Numeric random(Numeric from, Numeric to)
+{
+    thread_local static Generator gen(std::random_device{}());
+    using dist_type = typename std::conditional
+    <
+        std::is_integral<Numeric>::value
+        , std::uniform_int_distribution<Numeric>
+        , std::uniform_real_distribution<Numeric>
+    >::type;
+    thread_local static dist_type dist;
+    return dist(gen, typename dist_type::param_type{from, to});
+}
 // std::string readFile(const std::filesystem::path& path);
 
 #endif
