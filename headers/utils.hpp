4523cd6c6c5ffc4c7befe0629ac4fbb0c0dc37af
George Tian <github@georgetian.com>
2023-12-16 18:07:00 +0800

    2023-12-16 18:07:00

--- headers/utils.hpp
+++ headers/utils.hpp
@@ -2,53 +2,19 @@
 #define UTILS_HPP
 
 #include <filesystem>
-#include <fstream>
 #include <iostream>
 #include <random>
-#include <sstream>
 #include <string>
-#include <glm/glm.hpp>
 
 #define printLocation() std::cout << "########### printLocation: " << __FILE__ << ' ' << __LINE__ << " ###########\n";
 
 template <class T>
-inline T clamp(T value, T min, T max) {
-    return value < min ? min : value > max ? max : value;
-}
-
-inline std::string vec3ToString(glm::dvec3 vec) {
-    std::ostringstream stream;
-    stream << '(' << vec[0] << ',' << vec[1] << ',' << vec[2] << ')';
-    return stream.str();
-}
-
-inline glm::dvec3 applyModelMat(glm::dmat4 modelMat, glm::dvec3 point) {
-    return glm::dvec3(modelMat * glm::dvec4(point, 1.0));
-}
+inline T clamp(T value, T min, T max);
 
 // https://stackoverflow.com/a/35687575
 template<typename Numeric, typename Generator = std::mt19937>
-Numeric random(Numeric from, Numeric to)
-{
-    thread_local static Generator gen(std::random_device{}());
+Numeric random(Numeric from, Numeric to);
 
-    using dist_type = typename std::conditional
-    <
-        std::is_integral<Numeric>::value
-        , std::uniform_int_distribution<Numeric>
-        , std::uniform_real_distribution<Numeric>
-    >::type;
-
-    thread_local static dist_type dist;
-
-    return dist(gen, typename dist_type::param_type{from, to});
-}
-
-std::string readFile(const std::filesystem::path& path) {
-    std::ifstream f(path);
-    std::stringstream buffer;
-    buffer << f.rdbuf();
-    return buffer.str();
-}
+std::string readFile(const std::filesystem::path& path);
 
 #endif
