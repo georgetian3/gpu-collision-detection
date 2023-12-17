9842e7d6f52a3807634577e6fdd0b0b04e21f104
George Tian <github@georgetian.com>
2023-12-17 13:39:00 +0800

    2023-12-17 13:39:00

--- sources/collidable.cpp
+++ sources/collidable.cpp
@@ -27,7 +27,7 @@ std::vector<Collidable> Collidable::loadConfig(const std::filesystem::path& path
     // # s x y z r = sphere with center at (x, y, z) with radius r
     // # c x y z l = cube with smallest corner at (x, y, z) with side length l
     // # r x y z xl yl zl = rectangular cuboid with smallest corner at (x, y, z) with side lengths in x, y, z directions being xl, yl, yz, respectively
-    std::vector<std::shared_ptr<Collidable>> collidables;
+    std::vector<Collidable> collidables;
     std::ifstream f(path);
     if (!f.is_open()) {
         std::cout << "Cannot open file: " << path << '\n';
