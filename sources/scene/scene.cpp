05e35a63d7d63c1e063e2ffe9e3145ada16ba4b6
George Tian <github@georgetian.com>
2023-12-19 23:58:59 +0800

    2023-12-19 23:58:59

--- sources/scene/scene.cpp
+++ sources/scene/scene.cpp
@@ -22,6 +22,19 @@ Scene::Scene(const std::filesystem::path& cwd, int windowWidth, int windowHeight
     init();
 }
 
+const char *vertexShaderSource = "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}\0";
+const char *fragmentShaderSource = "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "void main()\n"
+    "{\n"
+    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "}\n\0";
+
 void Scene::render() {
 
     ///////////////////////////////////////////////////////////////////////
@@ -45,9 +58,50 @@ void Scene::render() {
         )
     );
     
-    shader.use();
+    // shader.use();
     // const Model cube = Model(cubeVertices, cubeIndices);
 
+
+    
+    // build and compile our shader program
+    // ------------------------------------
+    // vertex shader
+    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+    // check for shader compile errors
+    int success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+    // fragment shader
+    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+    // check for shader compile errors
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+    // link shaders
+    unsigned int shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    // check for linking errors
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+    }
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
 
     // set up vertex data (and buffer(s)) and configure vertex attributes
     // ------------------------------------------------------------------
@@ -88,49 +142,72 @@ void Scene::render() {
     glBindVertexArray(0); 
 
 
-    // glEnable(GL_DEPTH_TEST);
+    // uncomment this call to draw in wireframe polygons.
+    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
+    // render loop
+    // -----------
+    while (!glfwWindowShouldClose(window))
+    {
+        // input
+        // -----
+        processInput(window);
 
-    while (!glfwWindowShouldClose(window)) {
-
-        double currentTime = glfwGetTime();
-        double dt = currentTime - prevTime;
-        prevTime = currentTime;
-        processInput(window, dt);
-
-        frameCount++;
-        if (currentTime - prevFpsTime > updateInterval) {
-            fps = frameCount / (currentTime - prevFpsTime);
-            prevFpsTime = currentTime;
-            frameCount = 0;
-        }
-
-        glClearColor(0.5, 0.5, 0.5, 1.0);
+        // render
+        // ------
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        renderGui();
-
-        // shader.setMat4("model", glm::scale(glm::translate(glm::dmat4(1.0), glm::dvec3(0.0, 0.0, 10.0)), glm::dvec3(1)));
-        // shader.setMat4("view", camera.getView());
-        // shader.setMat4("projection", camera.getProjection());
-
-        // cube.draw();
-
+        // draw our first triangle
+        glUseProgram(shaderProgram);
         glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
         //glDrawArrays(GL_TRIANGLES, 0, 6);
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-
-        // collisionDetector->detectCollisions();
-
-        // if ((currentTime - startTime) * targetAnimationFps >= animationFrameCount) {
-            // glReadPixels(0, 0, windowWidth, windowHeight, GL_RGB, GL_UNSIGNED_BYTE, buf);
-            // animationFrameCount++;
-            // if (animationFrameCount % targetAnimationFps == 0) {
-            //     std::cout << animationFrameCount << '\n';
-            // }
-        // }
-
+        // glBindVertexArray(0); // no need to unbind it every time 
+ 
+        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+        // -------------------------------------------------------------------------------
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
+
+
+    // while (!glfwWindowShouldClose(window)) {
+
+    //     double currentTime = glfwGetTime();
+    //     double dt = currentTime - prevTime;
+    //     prevTime = currentTime;
+    //     processInput(window, dt);
+
+    //     frameCount++;
+    //     if (currentTime - prevFpsTime > updateInterval) {
+    //         fps = frameCount / (currentTime - prevFpsTime);
+    //         prevFpsTime = currentTime;
+    //         frameCount = 0;
+    //     }
+
+    //     glClearColor(0.5, 0.5, 0.5, 1.0);
+    //     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    //     renderGui();
+
+    //     shader.setMat4("model", glm::scale(glm::translate(glm::dmat4(1.0), glm::dvec3(0.0, 0.0, 10.0)), glm::dvec3(1)));
+    //     shader.setMat4("view", camera.getView());
+    //     shader.setMat4("projection", camera.getProjection());
+
+    //     cube.draw();
+
+    //     // collisionDetector->detectCollisions();
+
+    //     // if ((currentTime - startTime) * targetAnimationFps >= animationFrameCount) {
+    //         // glReadPixels(0, 0, windowWidth, windowHeight, GL_RGB, GL_UNSIGNED_BYTE, buf);
+    //         // animationFrameCount++;
+    //         // if (animationFrameCount % targetAnimationFps == 0) {
+    //         //     std::cout << animationFrameCount << '\n';
+    //         // }
+    //     // }
+
+    //     glfwSwapBuffers(window);
+    //     glfwPollEvents();
+    // }
 }
