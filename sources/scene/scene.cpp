aad5d2dc1482f5fe30c5b4b4a282a5b734b6f709
George Tian <github@georgetian.com>
2023-12-19 21:53:09 +0800

    2023-12-19 21:53:09

--- sources/scene/scene.cpp
+++ sources/scene/scene.cpp
@@ -61,7 +61,7 @@ void Scene::render() {
         // collisionDetector->detectCollisions();
 
         if ((currentTime - startTime) * targetAnimationFps >= animationFrameCount) {
-            glReadPixels(0, 0, windowWidth, windowHeight, GL_RGB, GL_UNSIGNED_BYTE, buf);
+            // glReadPixels(0, 0, windowWidth, windowHeight, GL_RGB, GL_UNSIGNED_BYTE, buf);
             // animationFrameCount++;
             // if (animationFrameCount % targetAnimationFps == 0) {
             //     std::cout << animationFrameCount << '\n';
