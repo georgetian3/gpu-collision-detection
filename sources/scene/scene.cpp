82077810e98b648d36612e383a589f1bc5c45107
George Tian <github@georgetian.com>
2023-12-20 00:11:13 +0800

    2023-12-20 00:11:13

--- sources/scene/scene.cpp
+++ sources/scene/scene.cpp
@@ -63,7 +63,6 @@ void Scene::render() {
 
 
 
-    vertex shader
     unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
     glCompileShader(vertexShader);
