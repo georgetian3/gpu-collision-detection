99cd30c5893fe464e18f0ee36c82e14bd98283da
George Tian <github@georgetian.com>
2023-12-19 14:40:46 +0800

    2023-12-19 14:40:46

--- sources/utils.cpp
+++ sources/utils.cpp
@@ -9,8 +9,10 @@ std::string readFile(const std::filesystem::path& path) {
     return buffer.str();
 }
 
-uint64_t expandBits3(double v) {
-    uint64_t u = v * (1ull << 63);
+uint64_t expandBits3(double v) { // assume v in range [0, 1]
+    uint64_t u = v * (1ull << 63); // make v = 0 -> u = 0...0, v = 1 -> u = 100....000
+    std::cout << toBits(u) << '\n';
+    u = u >> (64 - 21);
     std::cout << toBits(u) << '\n';
     u = (u * 0x00010001u) & 0xFF0000FFu;
     std::cout << toBits(u) << '\n';
