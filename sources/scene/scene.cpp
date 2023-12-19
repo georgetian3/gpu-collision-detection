753e02192a9e837d0ad84b1f29aecea951473fb7
George Tian <github@georgetian.com>
2023-12-20 00:20:54 +0800

    2023-12-20 00:20:54

--- sources/scene/scene.cpp
+++ sources/scene/scene.cpp
@@ -61,37 +61,10 @@ void Scene::render() {
     shader.use();
 
 
-    std::vector<float> vertices = {
-         0.5f,  0.5f, 0.0f,  // top right
-         0.5f, -0.5f, 0.0f,  // bottom right
-        -0.5f, -0.5f, 0.0f,  // bottom left
-        -0.5f,  0.5f, 0.0f   // top left 
-    };
-    std::vector<unsigned int> indices = {  // note that we start from 0!
-        0, 1, 3,  // first Triangle
-        1, 2, 3   // second Triangle
-    };
-    unsigned int VBO, VAO, EBO;
-
-
     const Model cube = Model(cubeVertices, cubeIndices);
 
 
 
-    // glGenVertexArrays(1, &VAO);
-    // glGenBuffers(1, &VBO);
-    // glGenBuffers(1, &EBO);
-    // // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
-    // glBindVertexArray(VAO);
-
-    // glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    // glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-    // glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    // glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-    // glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-    // glEnableVertexAttribArray(0);
 
 
 
@@ -119,9 +92,6 @@ void Scene::render() {
         // shader.setMat4("view", camera.getView());
         // shader.setMat4("projection", camera.getProjection());
 
-
-        //glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
-        //glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
         cube.draw();
 
         // collisionDetector->detectCollisions();
