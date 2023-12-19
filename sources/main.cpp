7c6317387f79d198e3389ac682dfa19d82b43aba
George Tian <github@georgetian.com>
2023-12-20 00:02:04 +0800

    2023-12-20 00:02:04

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,31 +1,9 @@
-// #include <scene.hpp>
-// #include <iostream>
-// #include <stopwatch.hpp>
-// #include <gpu_collision_detector.hpp>
-// #include <utils.hpp>
-// #include <glm/gtx/string_cast.hpp>
-
-// int main(int argc, char* argv[]) {
-//     if (argc > 1) {
-//         std::cout << "Command line arguments ignored\n";
-//     }
-
-//     // std::cout << "sizeof(Host Collidable): " << sizeof(Collidable) << '\n';
-
-//     // auto collidables = Collidable::loadConfig("resources/collidables.txt");
-//     // GpuCollisionDetector gpuCD;
-//     // gpuCD.setCollidables(collidables);
-//     // gpuCD.detectCollisions();
-
-//     Scene scene(
-//         (std::filesystem::current_path() / std::filesystem::path(argv[0]).parent_path()).lexically_normal(),
-//         1600, 900
-//     );
-
-//     scene.render();
-
-//     std::cout << "Done!";
-// }
+#include <scene.hpp>
+#include <iostream>
+#include <stopwatch.hpp>
+#include <gpu_collision_detector.hpp>
+#include <utils.hpp>
+#include <glm/gtx/string_cast.hpp>
 
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
@@ -41,8 +19,23 @@ const unsigned int SCR_HEIGHT = 600;
 
 
 
-int main()
-{
+int main(int argc, char* argv[]) {
+    if (argc > 1) {
+        std::cout << "Command line arguments ignored\n";
+    }
+
+    // std::cout << "sizeof(Host Collidable): " << sizeof(Collidable) << '\n';
+
+    // auto collidables = Collidable::loadConfig("resources/collidables.txt");
+    // GpuCollisionDetector gpuCD;
+    // gpuCD.setCollidables(collidables);
+    // gpuCD.detectCollisions();
+
+    Scene scene(
+        (std::filesystem::current_path() / std::filesystem::path(argv[0]).parent_path()).lexically_normal(),
+        1600, 900
+    );
+
 
     const char *vertexShaderSource = "#version 330 core\n"
     "layout (location = 0) in vec3 aPos;\n"
@@ -207,6 +200,14 @@ const char *fragmentShaderSource = "#version 330 core\n"
     // ------------------------------------------------------------------
     glfwTerminate();
     return 0;
+    std::cout << "Done!";
+}
+
+
+
+int main()
+{
+
 }
 
 // process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
