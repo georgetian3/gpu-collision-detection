bc642ccaae1f8483c18730eac5a2200f6928c78c
George Tian <github@georgetian.com>
2023-12-19 14:36:38 +0800

    2023-12-19 14:36:37

--- sources/utils.cpp
+++ sources/utils.cpp
@@ -10,7 +10,7 @@ std::string readFile(const std::filesystem::path& path) {
 }
 
 uint64_t expandBits3(double v) {
-    long long u = v * (1ll << 63);
+    unsigned long long u = v * (1ull << 63);
     std::cout << toBits(u) << '\n';
     u = (u * 0x00010001u) & 0xFF0000FFu;
     std::cout << toBits(u) << '\n';
