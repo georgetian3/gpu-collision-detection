7a367ac011e954cbb51f1093825c71d55842cf75
George Tian <github@georgetian.com>
2023-12-20 00:13:29 +0800

    2023-12-20 00:13:29

--- sources/scene/scene.cpp
+++ sources/scene/scene.cpp
@@ -115,8 +115,8 @@ void Scene::render() {
         // shader.setMat4("projection", camera.getProjection());
 
 
-        glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        //glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
+        //glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
         cube.draw();
 
         // collisionDetector->detectCollisions();
