3fc7ee1dec04c1b1914449249c41bbe2316f8b3a
George Tian <github@georgetian.com>
2023-12-20 00:19:40 +0800

    2023-12-20 00:19:39

--- headers/model.hpp
+++ headers/model.hpp
@@ -82,7 +82,8 @@ public:
 
     void draw() const {
         glBindVertexArray(VAO);
-        glDrawElements(GL_TRIANGLES, static_cast<unsigned int>(indices.size()), GL_UNSIGNED_INT, 0);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        //glDrawElements(GL_TRIANGLES, static_cast<unsigned int>(indices.size()), GL_UNSIGNED_INT, 0);
     }
 
 };
