d39ae6828c8ce6bb74068453dcac57fc7c5a404a
George Tian <github@georgetian.com>
2023-12-20 22:22:37 +0800

    2023-12-20 22:22:37

--- sources/gpu_collision_detector/detect_collisions.cpp
+++ sources/gpu_collision_detector/detect_collisions.cpp
@@ -35,8 +35,8 @@ std::vector<Collision> GpuCollisionDetector::detectCollisions() {
         std::cerr << "kernelConstruct exception: " << e.what();
         exit(1);
     }
-    nodes[collidables.size() - 1].left = nodes[collidables.size() - 2].right + 1;
-    nodes[collidables.size() - 1].right = collidables.size() * 2 - 1;
+    nodes[collidables.size() - 2].left = nodes[collidables.size() - 2].right + 1;
+    nodes[collidables.size() - 2].right = collidables.size() * 2 - 1;
     for (int i = 0; i < nodes.size(); i++) {
         printf("%02d, %02d, %02d", nodes[i].parent, nodes[i].left, nodes[i].right);
         if (i >= collidables.size() - 1) {
