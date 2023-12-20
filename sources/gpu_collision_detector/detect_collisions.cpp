4cf886148352c38b7e9dd564e5d102d5f83c93d7
George Tian <github@georgetian.com>
2023-12-20 22:17:39 +0800

    2023-12-20 22:17:39

--- sources/gpu_collision_detector/detect_collisions.cpp
+++ sources/gpu_collision_detector/detect_collisions.cpp
@@ -19,11 +19,6 @@ std::vector<Collision> GpuCollisionDetector::detectCollisions() {
         std::cout << collidable.toString() << '\n';
     }
 
-    std::vector<uint64_t> want;
-    for (const auto& collidable: collidables) {
-        want.push_back(morton3D(collidable.position.x, collidable.position.y, collidable.position.z));
-    }
-
     Stopwatch sw;
     sw.start();
     std::cout << "running kernel\n";
@@ -33,12 +28,6 @@ std::vector<Collision> GpuCollisionDetector::detectCollisions() {
     queue.enqueueReadBuffer(bufferCollidables, CL_TRUE, 0, sizeof(Collidable) * collidables.size(), collidables.data());
     std::cout << sw.stop() << '\n';
 
-    for (int i = 0; i < collidables.size(); i++) {
-        if (want[i] != collidables[i].mortonCode) {
-            std::cout << i << ' ' << toBits(want[i]) << ' ' << toBits(collidables[i].mortonCode) << '\n';
-            break;
-        }
-    }
 
     std::sort(collidables.begin(), collidables.end(), comp);
 
