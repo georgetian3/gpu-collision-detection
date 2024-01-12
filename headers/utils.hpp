953792906ffa3303a2cdb73f2d56dc07b6966ff7
George Tian <github@georgetian.com>
2024-01-12 15:39:17 +0800

    2024-01-12 15:39:17

--- headers/utils.hpp
+++ headers/utils.hpp
@@ -7,9 +7,10 @@
 #include <random>
 #include <sstream>
 #include <string>
+
 #define CL_HPP_ENABLE_EXCEPTIONS
-#define CL_HPP_TARGET_OPENCL_VERSION 120
-#define CL_HPP_MINIMUM_OPENCL_VERSION 120
+#define CL_HPP_TARGET_OPENCL_VERSION 300
+#define CL_HPP_MINIMUM_OPENCL_VERSION 300
 #include <CL/opencl.hpp>
 
 #define printLocation() std::cout << "########### printLocation: " << __FILE__ << ' ' << __LINE__ << " ###########\n";
