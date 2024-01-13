9ea52292c870154cbea9769d28a9cc5b61a80540
George Tian <github@georgetian.com>
2024-01-13 15:21:07 +0800

    2024-01-13 15:21:07

--- headers/gpu_collision_detector.hpp
+++ headers/gpu_collision_detector.hpp
@@ -2,8 +2,8 @@
 #define GPU_COLLISION_DETECTOR_HPP
 
 #define CL_HPP_ENABLE_EXCEPTIONS
-#define CL_HPP_TARGET_OPENCL_VERSION 300
-#define CL_HPP_MINIMUM_OPENCL_VERSION 300
+#define CL_HPP_TARGET_OPENCL_VERSION 200
+#define CL_HPP_MINIMUM_OPENCL_VERSION 200
 #include <CL/opencl.hpp>
 
 #include <collision_detector.hpp>
