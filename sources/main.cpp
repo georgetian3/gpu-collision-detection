cec616d21fdf65eceb14aa5f6138db8fa55cfa60
George Tian <github@georgetian.com>
2024-01-14 15:38:26 +0800

    2024-01-14 15:38:26

--- sources/main.cpp
+++ sources/main.cpp
@@ -42,7 +42,6 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
-#include <learnopengl/filesystem.h>
 #include <learnopengl/shader.h>
 #include <learnopengl/camera.h>
 #include <learnopengl/model.h>
