ee6c52709b94b7d74bc33f05c19d10c5016cede4
George Tian <github@georgetian.com>
2024-01-15 16:33:59 +0800

    2024-01-15 16:33:59

--- sources/scene/misc.cpp
+++ sources/scene/misc.cpp
@@ -66,7 +66,7 @@ void Scene::init() {
     ImGui_ImplOpenGL3_Init("#version 400");
 
     collidables = Collidable::loadConfig(makeAbsolute("resources/collidables.txt"));
-    Collidable ground = Collidable::constructCube(glm::dvec3(0.0), 1000);
+    Collidable ground = Collidable::constructCube(glm::dvec3(0.0), 100);
     ground.immovable = true;
     collidables.push_back(ground);
     gpuCD.setCollidables(collidables);
