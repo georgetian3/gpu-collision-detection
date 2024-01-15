2cad66e80a1e3b5ac6aab58e424161fa7199ab3c
George Tian <github@georgetian.com>
2024-01-15 15:24:25 +0800

    2024-01-15 15:24:25

--- sources/gpu_collision_detector/update_physics.cpp
+++ sources/gpu_collision_detector/update_physics.cpp
@@ -6,9 +6,34 @@
 
 void GpuCollisionDetector::updatePhysics(double dt) {
 
-    detectCollisions();
-
     try {
+        printLocation();
+        queue.enqueueNDRangeKernel(kernelMortonCode, cl::NullRange, cl::NDRange(collidables.size()));
+        printLocation();
+        queue.finish();
+        printLocation();
+        queue.enqueueReadBuffer(bufferCollidables, CL_TRUE, 0, sizeof(Collidable) * collidables.size(), collidables.data());
+        std::sort(collidables.begin(), collidables.end(), comp);
+        queue.enqueueWriteBuffer(bufferCollidables, CL_TRUE, 0, sizeof(Collidable) * collidables.size(), collidables.data());
+        printLocation();
+        queue.enqueueNDRangeKernel(kernelConstruct, cl::NullRange, cl::NDRange(collidables.size() - 1));
+        printLocation();
+        queue.finish();
+        printLocation();
+        queue.enqueueReadBuffer(bufferNodes, CL_TRUE, 0, sizeof(Node) * nodes.size(), nodes.data());
+        for (int i = 0; i < nodes.size(); i++) {
+            printf("%02d %02d %02d %02d %s %s", i, nodes[i].parent, nodes[i].left, nodes[i].right, glm::to_string(nodes[i].aabb.min).c_str(), glm::to_string(nodes[i].aabb.max).c_str());
+            if (i >= collidables.size() - 1) {
+                std::cout << ' ' << toBits(collidables[i - collidables.size() + 1].mortonCode) << ' ' << collidables[i - collidables.size() + 1].mortonCode;
+            }
+            std::cout << '\n';
+        }
+        queue.enqueueWriteBuffer(bufferProcessed, CL_TRUE, 0, sizeof(cl_int) * processed_zeros.size(), processed_zeros.data());
+        printLocation();
+        queue.enqueueNDRangeKernel(kernelAABB, cl::NDRange(collidables.size() - 1), cl::NDRange(collidables.size()));
+        printLocation();
+        queue.finish();
+        printLocation();
         kernelPhysics.setArg(1, sizeof(dt), &dt);
         printLocation();
         queue.enqueueNDRangeKernel(kernelPhysics, cl::NullRange, cl::NDRange(collidables.size()));
@@ -17,7 +42,6 @@ void GpuCollisionDetector::updatePhysics(double dt) {
         printLocation();
     } catch (const cl::Error& e) {
         printLocation();
-        std::cerr << "err\n";
         printClError(e);
     }
 
