1c5122bd4590b2159187a6f7b94f74d2b6075b39
George Tian <github@georgetian.com>
2024-01-16 15:13:16 +0800

    2024-01-16 15:13:16

--- sources/scene/misc.cpp
+++ sources/scene/misc.cpp
@@ -63,7 +63,7 @@ void Scene::init() {
 
     collidables = Collidable::loadConfig(makeAbsolute("resources/collidables.txt"));
     Collidable ground = Collidable::constructRectangularCuboid(glm::dvec3(0.0), 1, 0.05, 1);
-    Collidable ground2 = Collidable::constructRectangularCuboid(glm::dvec3(0.5), 1, 0.05, 1);
+    Collidable ground2 = Collidable::constructCube(glm::dvec3(1), 1);
     ground.immovable = true;
     ground2.immovable = true;
     collidables.push_back(ground);
