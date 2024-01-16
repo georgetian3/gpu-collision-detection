4c8fa167de3a1d0d3c5eb1aa1d5ad50b8dedc5f9
George Tian <github@georgetian.com>
2024-01-16 16:16:26 +0800

    2024-01-16 16:16:26

--- sources/gpu_collision_detector/update_physics.cpp
+++ sources/gpu_collision_detector/update_physics.cpp
@@ -26,14 +26,14 @@ void GpuCollisionDetector::updatePhysics(double dt) {
         queue.enqueueNDRangeKernel(kernelAABB, cl::NDRange(collidables.size() - 1), cl::NDRange(collidables.size()));
         queue.finish();
         queue.enqueueReadBuffer(bufferNodes, CL_TRUE, 0, sizeof(Node) * nodes.size(), nodes.data());
-        // for (int i = 0; i < nodes.size(); i++) {
-        //     printf("%02d %02d %02d %02d %s %s", i, nodes[i].parent, nodes[i].left, nodes[i].right, glm::to_string(nodes[i].aabb.min).c_str(), glm::to_string(nodes[i].aabb.max).c_str());
-        //     if (i >= collidables.size() - 1) {
-        //         std::cout << ' ' << toBits(collidables[i - collidables.size() + 1].mortonCode) << ' ' << collidables[i - collidables.size() + 1].mortonCode;
-        //     }
-        //     std::cout << "\n";
-        // }
-        // std::cout << '\n';
+        for (int i = 0; i < nodes.size(); i++) {
+            printf("%02d %02d %02d %02d %s %s", i, nodes[i].parent, nodes[i].left, nodes[i].right, glm::to_string(nodes[i].aabb.min).c_str(), glm::to_string(nodes[i].aabb.max).c_str());
+            if (i >= collidables.size() - 1) {
+                std::cout << ' ' << toBits(collidables[i - collidables.size() + 1].mortonCode) << ' ' << collidables[i - collidables.size() + 1].mortonCode;
+            }
+            std::cout << "\n";
+        }
+        std::cout << '\n';
         queue.enqueueNDRangeKernel(kernelTraverse, cl::NullRange, cl::NDRange(collidables.size()));
         queue.finish();
         std::cout << "traversal done\n\n\n";
