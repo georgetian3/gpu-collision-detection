8159f991766cb4ad40dd694cb75eb318b647505d
George Tian <github@georgetian.com>
2024-01-17 13:57:47 +0800

    2024-01-17 13:57:47

--- headers/collidable.hpp
+++ headers/collidable.hpp
@@ -38,7 +38,7 @@ struct Collidable {
     unsigned int id = -1;
     CollidableType type;
 
-    bool immovable;
+    bool immovable = false;
 
     glm::dvec3 position = glm::dvec3(0.0);
     uint64_t mortonCode = 0;
