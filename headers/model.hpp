3809c52f1f1feb0667541479fac17dcb403564cb
George Tian <github@georgetian.com>
2024-01-18 01:27:37 +0800

    2024-01-18 01:27:37

--- headers/model.hpp
+++ headers/model.hpp
@@ -4,7 +4,7 @@
 #include <glm/glm.hpp>
 #include <glad/glad.h> 
 #include <vector>
-
+#include <cmath>
 
 class Model {
 
@@ -118,69 +118,34 @@ void createSphere() {
     sphereIndices.clear();
 
     const float PI = acos(-1.0f);
-    const double radius = 0.5;
+    const double r = 1.0;
     const int sectorCount = 10;
     const int stackCount = 10;
-    float x, y, z, xy;                              // vertex position
-    float nx, ny, nz, lengthInv = 1.0f / radius;    // normal
-    float s, t;                                     // texCoord
 
-    float sectorStep = 2 * PI / sectorCount;
-    float stackStep = PI / stackCount;
-    float sectorAngle, stackAngle;
+    float x, y, z, phi, theta;
 
+    sphereVertices.push_back(0.0f);
+    sphereVertices.push_back(0.0f);
+    sphereVertices.push_back(1.0f);
 
-
-    for (int i = 0; i <= stackCount; ++i) {
-        stackAngle = PI / 2 - i * stackStep;        // starting from pi/2 to -pi/2
-        xy = radius * cosf(stackAngle);             // r * cos(u)
-        z = radius * sinf(stackAngle);              // r * sin(u)
-
-        // add (sectorCount+1) vertices per stack
-        // the first and last vertices have same position and normal, but different tex coords
-        for(int j = 0; j <= sectorCount; ++j)
-        {
-            sectorAngle = j * sectorStep;           // starting from 0 to 2pi
-
-            // vertex position
-            x = xy * cosf(sectorAngle);             // r * cos(u) * cos(v)
-            y = xy * sinf(sectorAngle);             // r * cos(u) * sin(v)
+    for (int stack = 1; stack < stackCount; stack++) {
+        phi = PI - 2 * PI * stack / stackCount;
+        z = r * sin(phi);
+        for (int sector = 0; sector < sectorCount; sector++) {
+            theta = 2 * PI * sector / sectorCount;
+            x = r * cos(phi) * cos(theta);
+            y = r * cos(phi) * sin(theta);
             sphereVertices.push_back(x);
             sphereVertices.push_back(y);
             sphereVertices.push_back(z);
-
-
         }
+
     }
 
-    // indices
-    //  k1--k1+1
-    //  |  / |
-    //  | /  |
-    //  k2--k2+1
-    unsigned int k1, k2;
-    for (int i = 0; i < stackCount; ++i) {
-        k1 = i * (sectorCount + 1);     // beginning of current stack
-        k2 = k1 + sectorCount + 1;      // beginning of next stack
+    sphereVertices.push_back(0.0f);
+    sphereVertices.push_back(0.0f);
+    sphereVertices.push_back(-1.0f);
 
-        for(int j = 0; j < sectorCount; ++j, ++k1, ++k2)
-        {
-            // 2 triangles per sector excluding 1st and last stacks
-            if(i != 0)
-            {
-                sphereIndices.push_back(k1);
-                sphereIndices.push_back(k2);
-                sphereIndices.push_back(k1 + 1);
-            }
-
-            if (i != (stackCount-1)) {
-                sphereIndices.push_back(k1 + 1);
-                sphereIndices.push_back(k2);
-                sphereIndices.push_back(k2 + 1);
-            }
-
-        }
-    }
 
 }
 
