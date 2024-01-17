0e8b7322d547c01b7e46edc81735a59c1127d3a0
George Tian <github@georgetian.com>
2024-01-17 14:18:03 +0800

    2024-01-17 14:18:03

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import random
 
 collidables = []
 
-for i in range(4):
+for i in range(64):
     collidables.append(f's {str(round(random.random(), 3)).ljust(5, "0")} {str(round(random.random(), 3)).ljust(5, "0")} {str(round(random.random(), 3)).ljust(5, "0")} 0.01')
 
 with open('resources/collidables.txt', 'w') as f:
