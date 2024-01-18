059449019ec78b213219bf3edca35797871655f2
George Tian <github@georgetian.com>
2024-01-18 21:03:05 +0800

    2024-01-18 21:03:05

--- sources/gpu_collision_detector/update_physics.cpp
+++ sources/gpu_collision_detector/update_physics.cpp
@@ -24,9 +24,9 @@ void GpuCollisionDetector::updatePhysics(double dt) {
         queue.enqueueReadBuffer(bufferNodes, CL_TRUE, 0, sizeof(Node) * nodes.size(), nodes.data());
         for (int i = 0; i < nodes.size(); i++) {
             printf("%02d %02d %02d %02d %s %s", i, nodes[i].parent, nodes[i].left, nodes[i].right, glm::to_string(nodes[i].aabb.min).c_str(), glm::to_string(nodes[i].aabb.max).c_str());
-            if (i >= collidables.size() - 1) {
-                std::cout << ' ' << toBits(collidables[i - collidables.size() + 1].mortonCode) << ' ' << collidables[i - collidables.size() + 1].mortonCode;
-            }
+            // if (i >= collidables.size() - 1) {
+            //     std::cout << ' ' << toBits(collidables[i - collidables.size() + 1].mortonCode) << ' ' << collidables[i - collidables.size() + 1].mortonCode;
+            // }
             std::cout << '\n';
         }
         queue.enqueueWriteBuffer(bufferProcessed, CL_TRUE, 0, sizeof(cl_int) * processed_zeros.size(), processed_zeros.data());
