a94bf377c472412091d736188492f3ce8580c87a
George Tian <github@georgetian.com>
2024-01-18 20:33:22 +0800

    2024-01-18 20:33:22

--- sources/gpu_collision_detector/gpu_collision_detector.cpp
+++ sources/gpu_collision_detector/gpu_collision_detector.cpp
@@ -237,7 +237,7 @@ void GpuCollisionDetector::setCollidables(const std::vector<Collidable>& collida
         kernelTraverse.setArg(2, bufferCollidables);
         kernelTraverse.setArg(3, bufferNodes);
 
-        glm::dvec3 gravity = glm::dvec3(0, -9.8, 0, 0);
+        glm::dvec3 gravity = glm::dvec3(0, -9.8, 0);
         kernelPhysics.setArg(0, bufferCollidables);
         kernelPhysics.setArg(2, sizeof(gravity), &gravity);
 
