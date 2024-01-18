ebf30739626f06ebe3b5c7909bc1251b1be0c9a9
George Tian <github@georgetian.com>
2024-01-18 21:02:43 +0800

    2024-01-18 21:02:43

--- sources/gpu_collision_detector/update_physics.cpp
+++ sources/gpu_collision_detector/update_physics.cpp
@@ -21,10 +21,6 @@ void GpuCollisionDetector::updatePhysics(double dt) {
         queue.enqueueWriteBuffer(bufferCollidables, CL_TRUE, 0, sizeof(Collidable) * collidables.size(), collidables.data());
         queue.enqueueNDRangeKernel(kernelConstruct, cl::NullRange, cl::NDRange(collidables.size() - 1));
         queue.finish();
-        queue.enqueueWriteBuffer(bufferProcessed, CL_TRUE, 0, sizeof(cl_int) * processed_zeros.size(), processed_zeros.data());
-        queue.enqueueNDRangeKernel(kernelAABB, cl::NDRange(collidables.size() - 1), cl::NDRange(collidables.size()));
-        queue.finish();
-        queue.enqueueReadBuffer(bufferNodes, CL_TRUE, 0, sizeof(Node) * nodes.size(), nodes.data());
         queue.enqueueReadBuffer(bufferNodes, CL_TRUE, 0, sizeof(Node) * nodes.size(), nodes.data());
         for (int i = 0; i < nodes.size(); i++) {
             printf("%02d %02d %02d %02d %s %s", i, nodes[i].parent, nodes[i].left, nodes[i].right, glm::to_string(nodes[i].aabb.min).c_str(), glm::to_string(nodes[i].aabb.max).c_str());
@@ -33,6 +29,9 @@ void GpuCollisionDetector::updatePhysics(double dt) {
             }
             std::cout << '\n';
         }
+        queue.enqueueWriteBuffer(bufferProcessed, CL_TRUE, 0, sizeof(cl_int) * processed_zeros.size(), processed_zeros.data());
+        queue.enqueueNDRangeKernel(kernelAABB, cl::NDRange(collidables.size() - 1), cl::NDRange(collidables.size()));
+        queue.finish();
         printLocation();
         queue.enqueueNDRangeKernel(kernelTraverse, cl::NullRange, cl::NDRange(collidables.size()));
         printLocation();
