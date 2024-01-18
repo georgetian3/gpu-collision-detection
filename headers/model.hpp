a785de627bb29b3e3c3c80658a716a0e9894194e
George Tian <github@georgetian.com>
2024-01-18 15:17:27 +0800

    2024-01-18 15:17:27

--- headers/model.hpp
+++ headers/model.hpp
@@ -120,7 +120,7 @@ void createSphere() {
     const float PI = acos(-1.0f);
 
     const double radius = 1.0;
-    const int sectorCount = 4, stackCount = 4;
+    const int sectorCount = 16, stackCount = 16;
     float x, y, z, xy;                              // vertex position
     float nx, ny, nz, lengthInv = 1.0f / radius;    // normal
     float s, t;                                     // texCoord
