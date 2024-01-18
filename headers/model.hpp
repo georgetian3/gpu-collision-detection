f537fe7537398a5e72187adbab3cf0e6fc68bc71
George Tian <github@georgetian.com>
2024-01-18 14:36:59 +0800

    2024-01-18 14:36:59

--- headers/model.hpp
+++ headers/model.hpp
@@ -119,8 +119,8 @@ void createSphere() {
 
     const float PI = acos(-1.0f);
     const double r = 1.0;
-    const int sectorCount = 4;
-    const int stackCount = 4;
+    const int sectorCount = 8;
+    const int stackCount = 8;
 
     float x, y, z, phi, theta;
 
