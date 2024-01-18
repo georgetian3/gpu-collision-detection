0385acb0743a9e147ecca329b1ff88056cbc72d1
George Tian <github@georgetian.com>
2024-01-18 20:57:41 +0800

    2024-01-18 20:57:41

--- sources/gpu_collision_detector/update_physics.cpp
+++ sources/gpu_collision_detector/update_physics.cpp
@@ -25,6 +25,14 @@ void GpuCollisionDetector::updatePhysics(double dt) {
         queue.enqueueNDRangeKernel(kernelAABB, cl::NDRange(collidables.size() - 1), cl::NDRange(collidables.size()));
         queue.finish();
         queue.enqueueReadBuffer(bufferNodes, CL_TRUE, 0, sizeof(Node) * nodes.size(), nodes.data());
+        queue.enqueueReadBuffer(bufferNodes, CL_TRUE, 0, sizeof(Node) * nodes.size(), nodes.data());
+        for (int i = 0; i < nodes.size(); i++) {
+            printf("%02d %02d %02d %02d %s %s", i, nodes[i].parent, nodes[i].left, nodes[i].right, glm::to_string(nodes[i].aabb.min).c_str(), glm::to_string(nodes[i].aabb.max).c_str());
+            if (i >= collidables.size() - 1) {
+                std::cout << ' ' << toBits(collidables[i - collidables.size() + 1].mortonCode) << ' ' << collidables[i - collidables.size() + 1].mortonCode;
+            }
+            std::cout << '\n';
+        }
         printLocation();
         queue.enqueueNDRangeKernel(kernelTraverse, cl::NullRange, cl::NDRange(collidables.size()));
         printLocation();
