b11d4c890ae196f0646659899ac7b29b6c09a642
George Tian <github@georgetian.com>
2024-01-19 14:31:35 +0800

    2024-01-19 14:31:35

--- shaders/model.fs
+++ shaders/model.fs
@@ -1,15 +1,10 @@
 R"(
     
 #version 330 core
-
-in vec3 Normal;  
-in vec3 FragPos;
-
 out vec4 FragColor;
 
-void main()
-{
-    FragColor = vec4(0.5, 0.5, 0.5, 1.0);
+void main() {
+    FragColor = vec4(1.0, 1.0, 1.0, 1.0);
 }
 
 
