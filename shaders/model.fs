f53fc2c64ebf6fa4eec250d1c7332f615b0bf021
George Tian <github@georgetian.com>
2024-01-19 14:34:16 +0800

    2024-01-19 14:34:16

--- shaders/model.fs
+++ shaders/model.fs
@@ -8,7 +8,7 @@ in vec3 FragPos;
 out vec4 FragColor;
 
 void main() {
-    const double brightness = 0.7;
+    const float brightness = 0.7;
     FragColor = vec4(brightness, brightness, brightness, 1.0);
 }
 
